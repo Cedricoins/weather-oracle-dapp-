c8b762994d1bc75dbca6b2528a11d0655af1fb5b
Cedricoins <111881437+Cedricoins@users.noreply.github.com>
2025-11-23 18:09:36 +0100

    Create page.tsx

---
+++ app/page.tsx
@@ -0,0 +1,9 @@
+import WeatherPredictor from "@/components/WeatherPredictor";
+
+export default function Home() {
+  return (
+    <main className="min-h-screen py-12">
+      <WeatherPredictor />
+    </main>
+  );
+}
